1da32c64598d4c453f6110b4ed7ffc7fd48c889b
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-11-03 22:31:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 // Your script here.
+function populateVoices() {
+    voices = speechSynthesis.getVoices();
+    voicesDropdown.innerHTML = voices
+        .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+        .join('');
+}
+
+// Set up the initial SpeechSynthesisUtterance properties
+function setUtteranceProperties() {
+    msg.voice = voices.find(voice => voice.name === voicesDropdown.value);
+    msg.text = document.querySelector('[name="text"]').value;
+    msg.rate = document.querySelector('[name="rate"]').value;
+    msg.pitch = document.querySelector('[name="pitch"]').value;
+}
+
+// Event listeners
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+
+voicesDropdown.addEventListener('change', setUtteranceProperties);
+
+options.forEach(option => {
+    option.addEventListener('input', () => {
+        setUtteranceProperties();
+    });
+});
+
+speakButton.addEventListener('click', () => {
+    speechSynthesis.speak(msg);
+});
+
+stopButton.addEventListener('click', () => {
+    speechSynthesis.cancel();
+});
